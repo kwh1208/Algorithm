14f91d7bffefec78be9e7d10ea0a925f7e38b3bf
heaming <meetoohi@gmail.com>
2024-04-12 13:28:24 +0900

    2024-04-12 * BOJ12893.java 1. 백준: 적의적 12893 https://www.acmicpc.net/problem/12893

--- Week3/조혜미/BOJ12893.java
+++ Week3/조혜미/BOJ12893.java
@@ -3,71 +3,76 @@
 @Title: 백준: 적의적 12893
 @Link: https://www.acmicpc.net/problem/12893
 @Author: hey._.mi
-
+@State : 20240412 retry
 */
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ12893 {
     public static int n;
     public static int m;
-    public static int[][] rels;
-    public static int[][] map;
+    public static int[] rels;
+    public static List<Integer>[] graph;
 
-    public static int dfs(int from, int to, int isEnemy) {
-
-        if(map[from][to] != isEnemy) {
-            return 0;
+    public static int dfs(int idx, int isEnemy) {
+        if(rels[idx] > -1) {
+            return rels[idx] == isEnemy ? 1 : 0;
         }
 
-        for(int i=0; i<=m; i++) {
-            if(map[from][i] == 1) {
-                dfs(i, 0,0);
+        rels[idx] = isEnemy;
+
+        for(int i=0; i < graph[idx].size(); i++) {
+            if(dfs(graph[idx].get(i), 1-isEnemy) == 0) {
+                return 0;
             }
         }
 
         return 1;
     }
 
-/*
-* 0000
-* 0011
-* 0001
-* 0100
-*
-1 2
-2 3
-1 3 0*/
+    public static void main(String[]args) throws IOException {
+        int answer = 1;
 
-    public static void main(String[]args)  throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            m = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken()); // 사람수
+        m = Integer.parseInt(st.nextToken()); // 적대관계수
 
-            rels = new int[m][2];
-            map = new int[n+1][n+1];
+        rels = new int[n+1]; // 적대관계
+        Arrays.fill(rels, -1);
+        graph = new List[n+1];
 
-            for (int i = 0; i < m; i++) {
-                st = new StringTokenizer(br.readLine());
-                rels[i][0] = Integer.parseInt(st.nextToken());
-                rels[i][1] = Integer.parseInt(st.nextToken());
-                map[rels[i][0]][rels[i][1]] = 1;
-                map[rels[i][1]][rels[i][0]] = 1;
-            }
-
-            for (int i=0; i < m; i++){
-                dfs(rels[i][0], rels[i][1], 1);
-            }
-
-           // bw.write(answer);
-
-            bw.flush();
-            br.close();
-            bw.close();
-
+        for(int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
         }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b= Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        for(int i=1; i <= n; i++) {
+            if(rels[i] != -1) continue;
+            if(dfs(i, 0) == 0) {
+                answer = 0;
+                break;
+            }
+        }
+
+        System.out.println(answer);
+
+
+       // bw.write(answer);
+
+        bw.flush();
+        br.close();
+        bw.close();
+
     }
+}
