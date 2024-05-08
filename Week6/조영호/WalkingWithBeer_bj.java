2cde583835cc765a8b8f72e965746c576ce0cfad
0hoooooo <youngho7482@naver.com>
2024-05-08 16:47:58 +0900

    걸으면서 맥주마시기_수정_백준

--- Week6/조영호/WalkingWithBeer_bj.java
+++ Week6/조영호/WalkingWithBeer_bj.java
@@ -1,19 +1,11 @@
 package Week6.조영호;
 /*문제*/
+
 // 송도에 사는 상근이와 친구들은 송도에서 열리는 펜타포트 락 페스티벌에 가려고 한다. 
 // 올해는 맥주를 마시면서 걸어가기로 했다. 출발은 상근이네 집에서 하고, 맥주 한 박스를 들고 출발한다. 
 // 맥주 한 박스에는 맥주가 20개 들어있다. 
 // 목이 마르면 안되기 때문에 50미터에 한 병씩 마시려고 한다. 
 // 즉, 50미터를 가려면 그 직전에 맥주 한 병을 마셔야 한다.
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
 // 상근이의 집에서 페스티벌이 열리는 곳은 매우 먼 거리이다. 
 // 따라서, 맥주를 더 구매해야 할 수도 있다. 
 // 미리 인터넷으로 조사를 해보니 다행히도 맥주를 파는 편의점이 있다. 
@@ -34,78 +26,95 @@ import java.util.StringTokenizer;
 
 /* 출력 */
 // 각 테스트 케이스에 대해서 상근이와 친구들이 행복하게 페스티벌에 갈 수 있으면 "happy", 중간에 맥주가 바닥나서 더 이동할 수 없으면 "sad"를 출력한다. 
-public class WalkingWithBeer_bj {
-    static class Point {
-		int r, c;
 
-		public Point(int r, int c) {
-			this.r = r;
-			this.c = c;
+import java.util.*;
+import java.io.*;
+
+public class WalkingWithBeer_bj {
+	// 결과
+	public static boolean[] result;
+	// 방문 여부 체크
+	public static boolean[] visited;
+
+	// 지점 클래스 선언
+	public static class Point {
+		int x;
+		int y;
+
+		public Point(int a, int b) {
+			x = a;
+			y = b;
 		}
 	}
 
-	static int TC, N;
-	static boolean visited[];
-	static int dr[] = { -1, 1, 0, 0 };
-	static int dc[] = { 0, 0, -1, 1 };
-	static String answer;
+	// DFS탐색
+	public static void DFS(Point now, ArrayList<Point> storeArr, Point festival, int current) {
+		// 페스티벌이 1000미터 내에 있는 지 확인
+		// 맥주 한 병당 50미터 이동할 수 있고 20병 있으므로.
+		if (Math.abs(now.x - festival.x) + Math.abs(now.y - festival.y) <= 1000) {
+			result[current] = true;
+			return;
+		}
+
+		// 편의점 위치 확인
+		for (int i = 0; i < storeArr.size(); i++) {
+			Point store = storeArr.get(i);
+
+			// 방문 안 한 편의점인 경우
+			if (visited[i] == false) {
+				// 1000미터 이내에 있다면 DFS 탐색 실행
+				if (Math.abs(now.x - store.x) + Math.abs(now.y - store.y) <= 1000) {
+					visited[i] = true;
+					DFS(store, storeArr, festival, current);
+				}
+			}
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// t : 테스트 케이스의 갯수
+		int t = Integer.parseInt(br.readLine());
+		result = new boolean[t];
+
 		StringTokenizer st;
-		TC = Integer.parseInt(br.readLine());
 
-		for (int t = 1; t <= TC; t++) {
-			N = Integer.parseInt(br.readLine());
-			ArrayList<Point> stores = new ArrayList<Point>();
-			Queue<Point> q = new ArrayDeque<Point>();
-			visited = new boolean[N];
-			answer = "sad";
+		for (int i = 0; i < t; i++) {
+			// n : 맥주를 파는 편의점의 갯수
+			int n = Integer.parseInt(br.readLine());
 
+			// home : 상근이 집 위치
 			st = new StringTokenizer(br.readLine());
-			int startC = Integer.parseInt(st.nextToken());
-			int startR = Integer.parseInt(st.nextToken());
-			// 집 좌표 q에 넣기
-			q.add(new Point(startR, startC));
+			Point home = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 
-			// 편의점 좌표 리스트에 넣기
-			for (int i = 0; i < N; i++) {
-
+			// store : 편의점 위치
+			ArrayList<Point> store = new ArrayList<>();
+			for (int j = 0; j < n; j++) {
 				st = new StringTokenizer(br.readLine());
-				int c = Integer.parseInt(st.nextToken());
-				int r = Integer.parseInt(st.nextToken());
-				stores.add(new Point(r, c));
+
+				store.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 			}
+
+			// festival : 축제 위치
 			st = new StringTokenizer(br.readLine());
-			int endC = Integer.parseInt(st.nextToken());
-			int endR = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
 
-			// bfs
-			while (!q.isEmpty()) {
-				Point cur = q.poll();
-				
-				// 현재 지점 (집 or 편의점) 에서 도착지점까지 갈수있으면 answer 설정 후 break 
-				if (Math.abs(cur.r - endR) + Math.abs(cur.c - endC) <= 1000) {
-					answer = "happy";
-					break;
-				}
-				
-				for (int i = 0; i < stores.size(); i++) {
-					Point next = stores.get(i);
-					
-					//이미 방문한 편의점이면 패스 
-					if(visited[i]) continue;
-					
-					//다음 편의점이  현재 지점(집 or 편의점)에서 갈수 있는 거리이면 방문체크하고 queue에 담기 
-					if(Math.abs(cur.r-next.r)+Math.abs(cur.c-next.c)<=1000) {
-						visited[i]=true;
-						q.add(new Point(next.r, next.c));
-					}
-				}
+			Point festival = new Point(x, y);
 
-			}
-			System.out.println(answer);
+			visited = new boolean[n];
 
+			// DFS탐색 시작
+			DFS(home, store, festival, i);
+		}
+
+		// 결과 출력
+		for (int i = 0; i < t; i++) {
+			if (result[i])
+				System.out.println("happy");
+			else
+				System.out.println("sad");
 		}
 	}
 }
