6cd6fc689d71c4e70d0b25feb911a7f2c21bb011
DESKTOP-RF4FDQ0\HP <ceo@dabitsol.com>
2024-07-22 16:31:23 +0900

    15주차 권우현 GCD합, 달팽이

---
+++ Week15/권우현/GCD합.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class GCD합 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer str;
+        for (int i = 0; i < t; i++) {
+            str = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(str.nextToken());
+            int[] num = new int[n];
+            for (int j = 0; j < n; j++) {
+                num[j] = Integer.parseInt(str.nextToken());
+            }
+            long ans = 0;
+            for (int j = 0; j < n; j++) {
+                for (int k = j+1; k < n; k++) {
+                    ans+=gcd(num[k], num[j]);
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+
+    private static long gcd(int a, int b){
+        int A = Math.max(a,b);
+        int B = Math.min(a,b);
+
+        while (true){
+            if(A%B == 0) return B;
+
+            int tmp = B;
+            B = A%B;
+            A = tmp;
+        }
+    }
+}
---
+++ Week15/권우현/달팽이.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class 달팽이 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int targetNum = sc.nextInt();
+
+        int[][] arr = new int[n][n];
+        int num = 1, move = 1;
+        int x = n/2, y = n/2;
+
+        while (true){
+            for (int i = 0; i < move; i++) {
+                arr[y--][x] = num++;
+            }
+
+            if (num>n*n) break;
+
+            for (int i = 0; i < move; i++) {
+                arr[y][x++] = num++;
+            }
+            move++;
+            for (int i = 0; i < move; i++) {
+                arr[y++][x] = num++;
+            }
+            for (int i = 0; i < move; i++) {
+                arr[y][x--] = num++;
+            }
+            move++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        int ans_x = 0;
+        int ans_y = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(arr[i][j]).append(" ");
+                if(arr[i][j] == targetNum){
+                    ans_x = i+1;
+                    ans_y = j+1;
+                }
+            }
+            sb.append("\n");
+        }
+        sb.append(ans_x).append(" ").append(ans_y);
+        System.out.println(sb.toString());
+    }
+}
