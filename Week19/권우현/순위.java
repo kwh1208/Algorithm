fcccdfc7cf6bb19c144ec295ac59b9f369865e3c
DESKTOP-RF4FDQ0\HP <ceo@dabitsol.com>
2024-08-21 21:27:55 +0900

    권우현 19주차 순위

--- Week19/권우현/순위.java
+++ Week19/권우현/순위.java
@@ -1,44 +1,35 @@
 package Week19.권우현;
 
 public class 순위 {
-    static class Solution {
+    class Solution {
         public int solution(int n, int[][] results) {
-            int[][] grade = new int[n][n];
-
-            for(int i = 0; i < results.length; i++){
-                grade[results[i][0] - 1][results[i][1] - 1] = -1;
-                grade[results[i][1] - 1][results[i][0] - 1] = 1;
+            int answer = 0;
+            boolean[][] grade = new boolean[n][n];
+            for(int i=0;i<results.length;i++){
+                grade[results[i][0]-1][results[i][1]-1] = true;
             }
 
 
-
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        for (int k = 0; k < n; k++) {
-                            if (grade[j][i] == -1 && grade[i][k] == -1) {
-                                grade[j][k] = -1;
-                                grade[k][j] = 1;
-                            }
-                            if (grade[j][i] == 1 && grade[i][k] == 1) {
-                                grade[j][k] = 1;
-                                grade[k][j] = -1;
-                            }
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    for (int k = 0; k < n; k++) {
+                        if(grade[j][i]&&grade[i][k]){
+                            grade[j][k] = true;
                         }
                     }
                 }
+            }
 
             int ans = 0;
+
             for (int i = 0; i < n; i++) {
                 int cnt = 0;
                 for (int j = 0; j < n; j++) {
-                    if (grade[i][j] != 0) {
-                        cnt++;
-                    }
+                    if(grade[i][j]||grade[j][i]) cnt++;
                 }
-                if (cnt == n - 1) {
-                    ans++;
-                }
+                if(cnt==n-1) ans++;
             }
+
 
             return ans;
         }
