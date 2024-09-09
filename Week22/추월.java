4e88dcf4eec2eaa4d53998a29c0a53106ff6c250
DESKTOP-RF4FDQ0\HP <ceo@dabitsol.com>
2024-09-09 16:38:27 +0900

    권우현 22주차 추월

---
+++ Week22/추월.java
@@ -0,0 +1,39 @@
+package Week22;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class 추월 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(br.readLine(), i);
+        }
+
+        int cnt = 0;
+        int[] out = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            out[i] = map.get(br.readLine());
+        }
+
+        // 추월 여부 계산
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (out[i] > out[j]) {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
