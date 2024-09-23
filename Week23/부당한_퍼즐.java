5bc12f7475ff95812ac94911f907bf78ce4b531c
DESKTOP-RF4FDQ0\HP <ceo@dabitsol.com>
2024-09-23 13:55:46 +0900

    권우현 23주차 부당한 퍼즐

---
+++ Week23/부당한_퍼즐.java
@@ -0,0 +1,64 @@
+package Week23;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 부당한_퍼즐 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] original = new int[n];
+        int[] puzzle = new int[n];
+
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            original[i] = Integer.parseInt(input[i]);
+        }
+
+        input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            puzzle[i] = Integer.parseInt(input[i]);
+        }
+
+        int first = original[0];
+        int num = 0;
+        for (int i = 0; i < n; i++) {
+            if (puzzle[i] == first) {
+                num = i;
+                break;
+            }
+        }
+
+        boolean isSame = true;
+
+        // 정방향 비교
+        for (int i = 0; i < n; i++) {
+            if (original[i] != puzzle[(num + i) % n]) {
+                isSame = false;
+                break;
+            }
+        }
+
+        if (isSame) {
+            System.out.println("good puzzle");
+            return;
+        }
+
+        // 역방향 비교
+        isSame = true;
+        for (int i = 0; i < n; i++) {
+            if (original[i] != puzzle[(num - i + n) % n]) {
+                isSame = false;
+                break;
+            }
+        }
+
+        if (isSame) {
+            System.out.println("good puzzle");
+        } else {
+            System.out.println("bad puzzle");
+        }
+    }
+}
